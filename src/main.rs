9850dee91e83420879b56b3ba4b152947b5a60a9
Brendan Perrine <walterorlin@gmail.com>
2015-10-27 11:36:19 -0700

    inital commit start with cargo configurtaion and a simple outline of drawing the truck and where the text should be

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("This is where the truck should talk. ");
+   println!("                                    ");
+   println!("       |    /-----------------------|                    ");
+   println!("       |   /                        |                    ");
+   println!("       |  /                         |                    ");
+   println!("    ___|_/                          |____________________");
+   println!("   /                                                    |");
+   println!("  |                                                     |");
+   println!("  |                                                     |");
+   println!("  \\          _________________________________      ____|");
+}

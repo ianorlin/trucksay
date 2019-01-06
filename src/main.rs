184c56eb55af73044b6593a53e793e6e921ce74b
Lyn Perrine <lynorian@lubuntu.me>
2019-01-05 20:20:13 -0800

    Improve truck proportions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     println!("This is where the truck should talk. ");
    println!("                                    ");
-   println!("       |    /-----------------------|                    ");
-   println!("       |   /                        |                    ");
-   println!("       |  /                         |                    ");
-   println!("    ___|_/                          |____________________");
-   println!("   /                                                    |");
-   println!("  |                                                     |");
-   println!("  |                                                     |");
-   println!("  \\          _________________________________      ____|");
+   println!("       |      /---------------------|                    ");
+   println!("       |     /                      |                    ");
+   println!("       |    /                       |                    ");
+   println!("    ___|___/                        |____________________");
+   println!("   /                                                     |");
+   println!("  |                                                      |");
+   println!("  |                                                      |");
+   println!("  \\           _________________________________      ____|");
 }
